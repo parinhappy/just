6db0339b2b37818e1a8f6d8edb1bbb6f5e404d84
parinhappy <59428885+parinhappy@users.noreply.github.com>
2021-11-02 21:26:33 +0700

    Initial commit

---
+++ src/components/AppHeader.js
@@ -0,0 +1,11 @@
+import './AppHeader.css';
+
+function AppHeader() {
+    return (
+        <header className="app-header">
+            <h7>Just tattoo</h7>
+        </header>
+    );
+}
+
+export default AppHeader;
